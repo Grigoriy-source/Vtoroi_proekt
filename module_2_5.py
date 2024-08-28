d6bd3f5b128febb2024ba4779ca078359abf3707
Гриня <grigoriy.korotkov.82@mail.ru>
2024-08-28 12:20:41 +0300

    Initial commit

---
+++ module_2_5.py
@@ -0,0 +1,12 @@
+def get_matrix(n, m, value):
+    matrix = []
+    for i in range(n):
+        a = []
+        for j in range(m):
+            a.append(value)
+            matrix.append(a)
+    return matrix
+
+result = get_matrix(3, 3, 5)
+print(result)
+
