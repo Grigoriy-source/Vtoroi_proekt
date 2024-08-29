c757a5395a5a0821763769910bb27e3f461091a3
Гриня <grigoriy.korotkov.82@mail.ru>
2024-08-29 13:28:41 +0300

    Initial commit

--- mobule_3_2.py
+++ mobule_3_2.py
@@ -1,15 +1,20 @@
-def send_email(mesenge, recipient, sender = "university.help@gmail.com"):
-    if ('@' and ('.com' or '.ru' or '.net')) not in (mesenge or recipient) or ('@' and ('.com' or '.ru' or '.net')) not in (mesenge or sender):
+def send_email(messange, recipient, sender = 'university.help@gmail.com'):
+    recipient.replace('.ru', '$')
+    recipient.replace('.com', '$')
+    recipient.replace('.net', '$')
+    messange.replace('.ru', '$')
+    messange.replace('.com', '$')
+    messange.replace('.net', '$')
+    if '$' in recipient or '$' in messange:
         print(f'Невозможно отправить письмо с адреса {sender} на адрес {recipient}')
     elif recipient == sender:
         print('Нельзя отправить письмо самому себе!')
-    elif sender == "university.help@gmail.com":
-        print('Письмо успешно отправлено с адреса {sender} на адрес {recipient}')
-    elif sender != "university.help@gmail.com":
-        print('НЕСТАНДАРТНЫЙ ОТПРАВИТЕЛЬ! Письмо отправлено с адреса {sender} на адрес {recipient}')
+    elif sender == 'university.help@gmail.com':
+        print(f'Письмо успешно отправлено с адреса {sender} на адрес {recipient}.')
+    elif sender != 'university.help@gmail.com':
+        print(f'НЕСТАНДАРТНЫЙ ОТПРАВИТЕЛЬ! Письмо отправлено с адреса {sender} на адрес {recipient}.')
+
 
 send_email('message','university.help@gmail.com','@.com')
 send_email('message','university.help@gmail.com')
 send_email('message','universi@.com')
-send_email('message','universi@.co')
-send_email('message','universi.com')
