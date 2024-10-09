d98f277abc92a5ccb5e85f8088594a3fe9531037
Гриня <grigoriy.korotkov.82@mail.ru>
2024-10-09 09:09:37 +0300

    Initial commit

--- module_5_3.py
+++ module_5_3.py
@@ -1,31 +1,72 @@
 class House:
-
-    houses_history = []
-
-    def __new__(cls, *args, **kwargs):
-        cls.houses_history.append(args[0])
-        return object.__new__(cls)
-
     def __init__(self, name, number_of_floors):
-        self.name = name
-        self.number_of_floors = number_of_floors
+       self.name = name
+       self.number_of_floors = number_of_floors
 
+    def __len__(self):
+         return self.number_of_floors
     def __str__(self):
-        return self.name
+       title = str(f'Название: {self.name}, кол-во этажей: {self.number_of_floors}')
+       return title
 
-    def __del__(self):
-        print(f'{self.name} снесён, но он останется в истории')
+    def __eq__(self, other):    # 1
+        if isinstance(other.number_of_floors, int) and isinstance(other, House):
+            return self.number_of_floors == other.number_of_floors
+
+    def __lt__(self, other):    # 2
+        if isinstance(other.number_of_floors, int) and isinstance(other, House):
+            return self.number_of_floors < other.number_of_floors
+
+    def __le__(self, other):
+        if isinstance(other.number_of_floors, int) and isinstance(other, House):
+            return self.number_of_floors <= other.number_of_floors
+
+    def __gt__(self, other):
+        if isinstance(other.number_of_floors, int) and isinstance(other, House):
+            return self.number_of_floors > other.number_of_floors
+
+    def __ge__(self, other):
+        if isinstance(other.number_of_floors, int) and isinstance(other, House):
+            return self.number_of_floors >= other.number_of_floors
+
+    def __ne__(self, other):
+        if isinstance(other.number_of_floors, int) and isinstance(other, House):
+            return self.number_of_floors != other.number_of_floors
+
+    def __add__(self, value):
+        if isinstance(value, int):
+            self.number_of_floors = self.number_of_floors + value
+        return self
+
+    def __radd__(self, value):
+        return self.__add__(value)
+
+    def __iadd__(self, value):
+        if isinstance(value, int):
+            self.number_of_floors += value
+        return self
 
 
 h1 = House('ЖК Эльбрус', 10)
-print(House.houses_history)
 h2 = House('ЖК Акация', 20)
-print(House.houses_history)
-h3 = House('ЖК Матрёшки', 20)
-print(House.houses_history)
 
-# Удаление объектов
-del h2
-del h3
+print(h1)
+print(h2)
 
-print(House.houses_history)
+print(h1 == h2) # __eq__
+
+h1 = h1 + 10 # __add__
+print(h1)
+print(h1 == h2)
+
+h1 += 10 # __iadd__
+print(h1)
+
+h2 = 10 + h2 # __radd__
+print(h2)
+
+print(h1 > h2) # __gt__
+print(h1 >= h2) # __ge__
+print(h1 < h2) # __lt__
+print(h1 <= h2) # __le__
+print(h1 != h2) # __ne__
