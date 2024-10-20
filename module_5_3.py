b28dbf55feb36c5e8e3a68a5bd3b92100bd7b0fc
Гриня <grigoriy.korotkov.82@mail.ru>
2024-10-20 10:21:57 +0300

    Initial commit

--- module_5_3.py
+++ module_5_3.py
@@ -1,51 +1,45 @@
 class House:
     def __init__(self, name, number_of_floors):
-       self.name = name
-       self.number_of_floors = number_of_floors
+        self.name = name # Имя
+        self.number_of_floors = number_of_floors # кол-во этаж
 
+    def go_to(self, new_floor):
+        if 0 < new_floor <= self.number_of_floors:
+            for floor in range(1, new_floor + 1):
+                print(floor)
+        else:
+            print('Такого этажа не сущесвует')
     def __len__(self):
-         return self.number_of_floors
-    def __str__(self):
-       title = str(f'Название: {self.name}, кол-во этажей: {self.number_of_floors}')
-       return title
+        return  self.number_of_floors
 
-    def __eq__(self, other):    # 1
+    def __str__(self):
+        return f'Название:{self.name}, кол-во этажей: {self.number_of_floors}'
+    def __eq__(self, other):
         if isinstance(other.number_of_floors, int) and isinstance(other, House):
             return self.number_of_floors == other.number_of_floors
-
-    def __lt__(self, other):    # 2
+    def __lt__(self, other):
         if isinstance(other.number_of_floors, int) and isinstance(other, House):
             return self.number_of_floors < other.number_of_floors
-
     def __le__(self, other):
         if isinstance(other.number_of_floors, int) and isinstance(other, House):
             return self.number_of_floors <= other.number_of_floors
-
     def __gt__(self, other):
         if isinstance(other.number_of_floors, int) and isinstance(other, House):
             return self.number_of_floors > other.number_of_floors
-
     def __ge__(self, other):
         if isinstance(other.number_of_floors, int) and isinstance(other, House):
             return self.number_of_floors >= other.number_of_floors
-
     def __ne__(self, other):
         if isinstance(other.number_of_floors, int) and isinstance(other, House):
             return self.number_of_floors != other.number_of_floors
-
-    def __add__(self, value):
+    def __add__(self, value) :
         if isinstance(value, int):
             self.number_of_floors = self.number_of_floors + value
         return self
-
     def __radd__(self, value):
         return self.__add__(value)
-
-    def __iadd__(self, value):
-        if isinstance(value, int):
-            self.number_of_floors += value
-        return self
-
+    def  __iadd__(self, value):
+        return self.__add__(value)
 
 
 h1 = House('ЖК Эльбрус', 10)
