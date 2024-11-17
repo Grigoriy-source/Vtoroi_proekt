3a2853a5a0d11f6abe9411da703c53702310be7d
Гриня <grigoriy.korotkov.82@mail.ru>
2024-11-17 20:01:04 +0300

    Initial commit

--- module_10_3.py
+++ module_10_3.py
@@ -1,55 +1,47 @@
 import threading
-import time
-import random
-from threading import Thread, Lock
+from random import randint
+from threading import Lock, Thread
+from time import sleep
 
 
 class Bank:
-
-    def __init__(self):
-        self.balance = 0
-        self.lock = Lock()
+    def __init__(self, balance: int = 0, lock: Lock = Lock()):
+        self.balance = balance  # баланс банка
+        self.lock = lock  # объект класса Lock для блокировки потоков
 
     def deposit(self):
-        count_iter = 0
-        while count_iter <= 99:
-            bk = random.randint(50, 500)
-            self.lock.acquire()
-            try:
-                self.balance += bk
-                print(f'Пополнение: {bk}. Баланс: {self.balance}\n')
-            finally:
+        for i in range(100):
+            transfer = randint(50, 500)
+            self.balance += transfer
+            print(f'Пополнение: {transfer}. Баланс: {self.balance}')
+            if self.balance >= 500 and self.lock.locked():
                 self.lock.release()
-            time.sleep(0.01)
-            count_iter += 1
+            sleep(0.001)
 
     def take(self):
-        count_iter = 0
-        while count_iter <= 99:
-            bk = random.randint(50, 500)
-            print(f'Запрос на {bk}')
-            self.lock.acquire()
-            try:
-                if bk <= self.balance:
-                    self.balance -= bk
-                    print(f'Снятие: {bk}. Баланс: {self.balance}\n')
-                else:
-                    print('Запрос отклонён, недостаточно средств')
-            finally:
-                self.lock.release()
-            time.sleep(0.01)
-            count_iter += 1
+        for i in range(100):
+            transfer = randint(50, 500)
+            print(f'Запрос на {transfer}')
+            if transfer <= self.balance:
+                self.balance -= transfer
+                print(f'Снятие: {transfer}. Баланс: {self.balance}')
+            else:
+                print('Запрос отклонён, недостаточно средств')
+                self.lock.acquire()
+            sleep(0.001)
+
+if __name__ == '__main__':
+    bk = Bank()
+
+    # Т.к. методы принимают self, в потоки нужно передать сам объект класса Bank
+    th1 = threading.Thread(target=Bank.deposit, args=(bk,))
+    th2 = threading.Thread(target=Bank.take, args=(bk,))
+
+    th1.start()
+    th2.start()
+    th1.join()
+    th2.join()
+
+    print(f'Итоговый баланс: {bk.balance}')
 
 
-bk = Bank() # создаем объект класса
-
-# Т.к. методы принимают self, в потоки нужно передать сам объект класса Bank
-th1 = threading.Thread(target=Bank.deposit, args=(bk,))
-th2 = threading.Thread(target=Bank.take, args=(bk,))
-
-th1.start()
-th2.start()
-th1.join()
-th2.join()
-
-print(f'Итоговый баланс: {bk.balance}')
