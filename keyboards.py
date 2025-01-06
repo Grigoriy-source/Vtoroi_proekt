b3c3f018d7da7aa47d02a9b90aa57d0df90a674e
Гриня <grigoriy.korotkov.82@mail.ru>
2025-01-06 11:32:13 +0300

    Initial commit

---
+++ keyboards.py
@@ -0,0 +1,32 @@
+from aiogram.types import ReplyKeyboardMarkup,KeyboardButton
+from aiogram.types import InlineKeyboardMarkup,InlineKeyboardButton
+
+
+
+kb = ReplyKeyboardMarkup(
+    keyboard=[
+        [
+            KeyboardButton(text='Рассчитать'),
+            KeyboardButton(text='Информация')
+        ],
+        [KeyboardButton(text='Купить')]
+    ], resize_keyboard=True
+)
+
+inline_kb = InlineKeyboardMarkup(
+    inline_keyboard=[
+        [
+            InlineKeyboardButton(text='Рассчитать норму калорий',callback_data='calories'),
+            InlineKeyboardButton(text='Формулы расчёта', callback_data='formulas')
+        ]
+    ]
+)
+
+goods_kb = InlineKeyboardMarkup(
+    inline_keyboard=[[
+        InlineKeyboardButton(text='Product1', callback_data='product_buying'),
+        InlineKeyboardButton(text='Product2', callback_data='product_buying'),
+        InlineKeyboardButton(text='Product3', callback_data='product_buying'),
+        InlineKeyboardButton(text='Product4', callback_data='product_buying')
+    ]]
+)
